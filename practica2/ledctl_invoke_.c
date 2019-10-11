8d7eb3c3d08c02827772fed7aee070b082779aec
azohc <juanchoz@ucm.es>
2019-10-11 14:39:42 +0200

    totest

--- practica2/ledctl_invoke_.c
+++ practica2/ledctl_invoke_.c
@@ -2,38 +2,28 @@
 #include <sys/syscall.h>
 #include <linux/unistd.h> 
 #include <stdlib.h> 
-#include <stdio.h> 
+#include <stdio.h>
+#define __NR_gettid 332
 
-// #define __NR_gettid 332
 extern int errno ;
 
-// long ledctl(void) {
-// 	return (long) syscall(__NR_gettid);
-// }
+long ledctl(void) {
+	return (long) syscall(__NR_gettid);
+}
 
 int main (int argc, char *argv[]) {
 	char * buf;
-	unsigned int dist, numb;
+	unsigned int numb;
 	char * aux;
 	if (argc != 1) {
 		perror("Usage: ./ledctl_invoke <ledmask>");
 		return -1;
 	} 
-	buf= argv[0];
-	if (strlen(argv[0]) > 2){
-		if (argv[0][0] == 0 && argv[0][1] == 'x'){
-			dist = strlen(argv[0])-2;
-			memcpy(aux, &argv[0][2], dist;
-			aux[dist]= '\0';
-			buf = aux;
-		}
 
-	}
+	numb = strtoul(argv[0], NULL, 16);
 
-	numb = strtoul(buf, NULL, 10);
-
-	if(ledctl(num) == -1)
-		perror("Se ha producido un error.");
+	if(ledctl(numb) == -1)
+		perror("Se ha producido un error: %d", errno);
 	
 	return 0;
 }
