ff4faf83620d75e7e20a93cf86a51a6ff80e7c78
kropotkin8 <pablojc1998@gmail.com>
2019-10-17 23:09:05 +0200

    ready

---
+++ practica3/Practica3/ParteA/.blinkdrv.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/kernel/lin/practica3/Practica3/ParteA/blinkdrv.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/kernel/lin/practica3/Practica3/ParteA/blinkdrv.ko /home/kernel/lin/practica3/Practica3/ParteA/blinkdrv.o /home/kernel/lin/practica3/Practica3/ParteA/blinkdrv.mod.o ;  true
