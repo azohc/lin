69b7a04df994ce8a5136fe8327d8f0f8a2ca14a0
azohc <juanchoz@ucm.es>
2019-11-08 15:44:25 +0100

    pseudo ready

--- practica4/productor.sh
+++ practica4/productor.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-
+DIR="/proc/m0dLiSt"
 productor() {
 	for i in $(seq $1)
 	do
-		echo $i
-		# print timestamp
-		# insertar i
-		# cat lista
+		echo -n "PROD $i:" `date +%H:%M:%S` 
+		echo "add $i" > $DIR
 	done
 }
 
 consumidor() {
 	for i in $(seq $1)
 	do
-		echo 1
-		# print timestamp
-		# insertar i
-		# cat lista
+		echo -n "CONS $i:" `date +%H:%M:%S` 
+		echo "remove $i" > $DIR
+		echo " elems: $(cat $DIR)"
 	done
 }
 
